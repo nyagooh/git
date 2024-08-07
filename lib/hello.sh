05e9a22692d21916dd0f56f61c13e0f51078f36e
nymaina <annmaina614@gmail.com>
2024-08-07 12:07:13 +0300

    Add greet script with default name handling

--- lib/hello.sh
+++ lib/hello.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 echo "What's your name"
 read my_name
 
 echo "Hello, $my_name"
+=======
+source lib/greeter.sh
+
+name="$1"
+if [ -z "$name" ]; then
+    name="World"
+fi
+
+Greeter "$name"
+>>>>>>> b4046f1 (Add greet script with default name handling)
